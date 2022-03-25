aef97ba69eda32c5b249dc153efdddd882b82b05
mpenpav533 <94712851+mpenpav533@users.noreply.github.com>
2022-03-25 16:57:16 +0100

    Delete funciones directory

--- funciones/src/funciones/biblioteca_arrays.java
+++
@@ -1,8 +0,0 @@
-package funciones;
-
-public class biblioteca_arrays {
-public static void generaArryaInt(int array[]) {
-	//.generaArrayInt: Genera un array de tamaño n con números aleatorios cuyo intervalo (mínimo y
-	//máximo) se indica como parámetro.
-}
-}
